511c75e211660936786be21718eb78a2a92ab7b7
ErdosXenia <erdos.xenia@gmail.com>
2020-09-08 11:16:45 +0200

    Szamitogep valasztasa

--- 01Ismetles/Program.cs
+++ 01Ismetles/Program.cs
@@ -10,7 +10,12 @@ namespace _01Ismetles
     {
         static void Main(string[] args)
         {
-            
+            Random vel = new Random();
+            string[] lehetoseg = new string[] {"kő", "papír", "olló"};
+
+            int gepvalasz = vel.Next(0, 3);
+
+            Console.WriteLine("Gép választása: {0}", lehetoseg[gepvalasz]);
 
             Console.ReadKey();
         }
